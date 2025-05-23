4b642f45c6e0a931e6d97144444e8da35f81d10b
Harshwardhan Vishwakarma <hvishwakarma1729@gmail.com>
2025-05-23 18:50:26 +0530

    exercise 1.1 and 1.2 added
    made courseinfo dir solution for 1.1 and 1.2

--- Part1/introdemo/src/App.jsx
+++ Part1/introdemo/src/App.jsx
@@ -1,17 +1,47 @@
-const Hello = () => {
+const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
     </div>
   )
 }
 
 const App = () => {
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
+
+  const friend_arr = [ 'Peter', 'Maya']
+
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello />
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+
+      <Hello name={friends[0].name} age={friends[0].age} />
+      <Hello name={friends[1].name} age={friends[1].age} />
+
+      <p>{friend_arr}</p>
+
     </div>
   )
 }
+
+
+
 export default App
