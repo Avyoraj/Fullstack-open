c209a1af6e9104bde6150b9368da03d761ad09ff
Harshwardhan Vishwakarma <hvishwakarma1729@gmail.com>
2025-05-27 21:03:02 +0530

    solved exercise 1.3-1.5
    used map in the content component
    
    since i felt the lines was same and not a good practice to hard code values
    
    also changed the console log for my better understanding in console

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,48 +1,56 @@
 const Header = (props) => {
-   console.log(props)
+  console.log(props)
   return <h1>{props.course}</h1>
+  
 }
 
 const Part = (props) => {
-  console.log(props)
-  return <p>{props.Part_name} {props.exercises}</p>
+  return <p>{props.name} {props.exercises}</p>
 }
 
-const Content = (props) => {
+const Content = ({ parts }) => {
+  console.log( { parts })
   return (
     <div>
-      <Part Part_name={props.part1} exercises={props.exercises1} />
-      <Part Part_name={props.part2} exercises={props.exercises2} />
-      <Part Part_name={props.part3} exercises={props.exercises3} />
+     { parts.map((part, index) => (
+        <Part key={index} name={part.name} exercises={part.exercises} />
+      ))}
     </div>
   )
 }
 
 const Total = (props) => {
-  console.log(props)
-  return <p>Number of exercises {props.total}</p>
+  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
+  console.log({total})
+  return <p>Number of exercises {total}</p>
 }
 
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content 
-        part1={part1} exercises1={exercises1} 
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
